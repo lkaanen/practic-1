8993c059c43a968badbcda09c8ee04283e380551
lkaanen <romvik20@gmail.com>
2024-01-17 09:36:57 +0700

    4 commit

--- js/main.js
+++ js/main.js
@@ -13,14 +13,27 @@ var app = new Vue({
       variants: [
         {
           variantId: 2234,
-          variantColor: 'green'    
+          variantColor: 'green'  
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'  
         },
         {
           variantId: 2235,
           variantColor: 'blue'
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
         }
       ],
       sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-        inventory: 100
+      cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      },
+      removeFromCart() {
+        this.cart -= 1
+      }
     }
 })
